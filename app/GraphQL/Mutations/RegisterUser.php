2e9055c9a4b3d64041fb2025882e510d52dc7062
Miles Wu <slion0725@gmail.com>
2021-12-21 17:15:35 +0800

    upd

---
+++ app/GraphQL/Mutations/RegisterUser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Facades\Hash;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class RegisterUser
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  @param  null  $root Always null, since this field has no parent.
+     * @param  array<string, mixed>  $args The field arguments passed by the client.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Shared between all fields.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Metadata for advanced query resolution.
+     * @return mixed
+     */
+    public function __invoke($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        $user = User::create([
+            'name' => $args['name'],
+            'email' => $args['email'],
+            'password' => Hash::make($args['password']),
+        ]);
+
+        // wip:send-verify-email
+
+        return $user;
+    }
+}
