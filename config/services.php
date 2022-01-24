d2fbb4e14bbd488db58e8e180e01cb3d7bafa6a1
Miles Wu <slion0725@gmail.com>
2022-01-24 17:33:59 +0800

    upd

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => 'http://localhost/auth/callback',
     ],
+
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => 'http://localhost/auth/callback',
+    ],
 ];
