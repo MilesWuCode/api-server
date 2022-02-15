3420372c182c596be647d7ca01b7dae6407b9dc5
Miles Wu <slion0725@gmail.com>
2022-02-15 12:03:40 +0800

    upd

---
+++ tests/Feature/FileTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Tests\TestCase;
+
+class FileTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_upload()
+    {
+        $response = $this->json('POST', '/api/demo-upload', [
+            'file' => UploadedFile::fake()->image('test.jpg')->size(600)
+        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'name',
+            'type',
+            'size',
+        ]);
+    }
+}
