abfa0fa56676efdded8f11cc9f7eaaa323c93259
Miles Wu <slion0725@gmail.com>
2022-03-21 11:59:43 +0800

    add:type

--- app/Transformers/MediaTransformer.php
+++ app/Transformers/MediaTransformer.php
@@ -12,7 +12,7 @@ class MediaTransformer extends TransformerAbstract
      *
      * @var array
      */
-    protected $defaultIncludes = [
+    protected array $defaultIncludes = [
         //
     ];
 
@@ -21,7 +21,7 @@ class MediaTransformer extends TransformerAbstract
      *
      * @var array
      */
-    protected $availableIncludes = [
+    protected array $availableIncludes = [
         //
     ];
 
